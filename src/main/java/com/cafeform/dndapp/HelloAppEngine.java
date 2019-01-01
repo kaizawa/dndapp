d80e9b6653916de470bf7cec67f561b33bdcc1a6
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2019-01-01 12:56:28 +0900

    first commit

---
+++ src/main/java/com/cafeform/dndapp/HelloAppEngine.java
@@ -0,0 +1,32 @@
+/**
+ * Not used. Can delete
+ */
+package com.cafeform.dndapp;
+
+import java.io.IOException;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    name = "HelloAppEngine",
+    urlPatterns = {"/hello"}
+)
+
+public class HelloAppEngine extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+@Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws IOException {
+
+    response.setContentType("text/plain");
+    response.setCharacterEncoding("UTF-8");
+
+    response.getWriter().print("Hello App Engine!\r\n");
+
+  }
+}
